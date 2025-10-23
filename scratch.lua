4799008db6fe0d8a1b4350a0ec36f06503f78e8e
Nick Rothwell <nick@cassiel.com>
2025-10-23 21:00:19 +0100

    Testing.

--- scratch.lua
+++ scratch.lua
@@ -3,6 +3,10 @@
 local inspect = require "launchpads.inspect"
 local my_midi
 
+local function data_byte(g, r)
+    return (g << 4) + (3 << 2) + r
+end
+
 function init()
     my_midi = midi.connect(1)
     
@@ -10,6 +14,44 @@ function init()
         local msg = midi.to_msg(data)
         print(inspect.inspect(msg))
         
-        my_midi:send(data)
+        if msg.type == "note_on" then
+            local v = data_byte(3, 1)
+            
+            --[[
+            my_midi:send(midi.to_data{
+                type = "note_on",
+                ch = 1,
+                note = msg.note,
+                vel = v
+            })
+            ]]
+        
+            for i = 1, 32 do
+                my_midi:send(midi.to_data{
+                    type = "note_on",
+                    ch = 3,
+                    note = data_byte(math.random(0, 3), math.random(0, 3)),
+                    vel = data_byte(math.random(0, 3), math.random(0, 3))
+                })
+            end
+        else
+            my_midi:send(data)
+        end
     end
+    
+    -- Reset:
+    my_midi:send(midi.to_data{
+        type = "cc",
+        ch = 1,
+        cc = 0,
+        val = 0
+    })
+
+    -- X/Y layout:
+    my_midi:send(midi.to_data{
+        type = "cc",
+        ch = 1,
+        cc = 0,
+        val = 1
+    })
 end
