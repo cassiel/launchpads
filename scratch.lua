a585dceb847d61351487e6ea88cb73762e60a533
Nick Rothwell <nick@cassiel.com>
2025-10-24 20:05:45 +0100

    Tweaks.

--- scratch.lua
+++ scratch.lua
@@ -1,6 +1,6 @@
 -- Tests for Novation Launchpad mini (mk 2 and mk 3).
 
-local inspect = require "launchpads.inspect"
+local inspect = require "launchpads.lib.inspect"
 local my_midi
 
 local function data_byte(g, r)
@@ -15,25 +15,38 @@ function init()
         print(inspect.inspect(msg))
         
         if msg.type == "note_on" then
-            local v = data_byte(3, 1)
-            
-            --[[
-            my_midi:send(midi.to_data{
-                type = "note_on",
-                ch = 1,
-                note = msg.note,
-                vel = v
-            })
-            ]]
-        
+            -- Central 8x8:
             for i = 1, 32 do
-                my_midi:send(midi.to_data{
-                    type = "note_on",
-                    ch = 3,
-                    note = data_byte(math.random(0, 3), math.random(0, 3)),
-                    vel = data_byte(math.random(0, 3), math.random(0, 3))
-                })
+                my_midi:note_on(
+                    data_byte(math.random(0, 3), math.random(0, 3)),
+                    data_byte(math.random(0, 3), math.random(0, 3)),
+                    3
+                )
             end
+            
+            -- Scenes, top=to-bottom:
+            for i = 1, 4 do
+                my_midi:note_on(
+                    data_byte(i - 1, 0),
+                    data_byte(i - 1, 0),
+                    3
+                )
+            end
+            
+            -- Automap, left-to-right:
+            for i = 1, 4 do
+                my_midi:note_on(
+                    data_byte(0, i - 1),
+                    data_byte(0, i - 1),
+                    3
+                )
+            end
+            
+            -- Need to reset the 2-up mode, so re-transmit
+            -- the first button (or, for testing, re-randomise):
+            my_midi:note_on(
+                0, data_byte(math.random(0, 3), math.random(0, 3)), 1
+            )
         else
             my_midi:send(data)
         end
